f864156746dfabda83b555e2da4c9b13460c13b4
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-05-14 21:16:49 -0300

    Adding UI to Login Screen

---
+++ instagram-clone/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  instagram-clone
+//
+//  Created by Fernando Moreira on 14/05/21.
+//  Copyright © 2021 Fernando Moreira. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+       return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
